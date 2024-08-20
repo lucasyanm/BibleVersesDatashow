b4603595dee4e71227f283b791a4c8c2a1d7f61e
Lucas Carvalho <lucasyancarvalho@gmail.com>
2024-08-19 21:08:42 -0300

    feature: search book by abbrev

---
+++ BibleVersesDatashow/Model/BibleBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BibleVersesDatashow.Model
+{
+    public class BibleBook
+    {
+        public required string abbrev { get; set; }
+        public required List<string> chapters { get; set; }
+        public required string name { get; set; }
+    }
+}
--- BibleVersesDatashow/ViewModel/MainViewModel.cs
+++ BibleVersesDatashow/ViewModel/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using BibleVersesDatashow.Model;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Reflection;
+using System.Text.Json;
 
 namespace BibleVersesDatashow.ViewModel
 {
@@ -22,6 +24,69 @@ namespace BibleVersesDatashow.ViewModel
 
             Console.WriteLine(FontSize);
             _style.FontSize = FontSize;
+        }
+
+        [ObservableProperty]
+        string abbrevToSearch;
+        [ObservableProperty]
+        string nameToSearch;
+        [ObservableProperty]
+        BibleBook currentBook;
+
+        [RelayCommand]
+        public async void GetBook()
+        {
+            if (String.IsNullOrEmpty(AbbrevToSearch) && String.IsNullOrEmpty(NameToSearch))
+            {
+                throw new ArgumentNullException("Informe o nome ou abreviação do livro que queira obter");
+            }
+
+            using (Stream fileStream = await Microsoft.Maui.Storage.FileSystem.OpenAppPackageFileAsync("Resources/Bible/nvi.json"))
+            {
+                using (JsonDocument jsonDocument = JsonDocument.Parse(fileStream))
+                {
+                    JsonElement root = jsonDocument.RootElement;
+                    if (root.ValueKind == JsonValueKind.Array)
+                    {
+                        foreach (JsonElement jsonElement in root.EnumerateArray())
+                        {
+                            if (!String.IsNullOrEmpty(NameToSearch)
+                                && jsonElement.TryGetProperty("name", out JsonElement nameProperty))
+                            {
+                                string jsonElementName = nameProperty.ToString();
+                                if (jsonElementName == NameToSearch)
+                                {
+                                    BibleBook? book = JsonSerializer.Deserialize<BibleBook>(jsonElement);
+
+                                    if (book != null)
+                                    {
+                                        CurrentBook = book;
+                                    }
+                                }
+                            }
+                            else if (jsonElement.TryGetProperty("abbrev", out JsonElement abbrevProperty))
+                            {
+                                string jsonElementAbbrev = abbrevProperty.ToString();
+                                if (jsonElementAbbrev == AbbrevToSearch)
+                                {
+                                    BibleBook? book = JsonSerializer.Deserialize<BibleBook>
+                                        (
+                                            jsonElement.ToString(), 
+                                            new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+                                        );
+
+                                    if (book != null)
+                                    {
+                                        CurrentBook = book;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            throw new Exception("Livro não encontrado");
         }
     }
 }
